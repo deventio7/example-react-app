f125b788e139f6da8af358e44f32fb8f0f3ff741
Alex Lee <deventio7@gmail.com>
2019-01-17 18:14:25 -0500

    allow CORS on backend

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,11 @@ let app = require('express')();
 log.setLevel('debug');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 let init = async () => {
     let state = {
